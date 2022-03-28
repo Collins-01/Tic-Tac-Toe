8895e0870254b119f1437ab6edb5cdd94a0414f7
Collins Oriakhi <collins.oriakhi@itara.co>
2022-03-28 17:50:11 +0100

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Oriakhi Collins on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
