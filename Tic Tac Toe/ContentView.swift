706937380058a5eb3fbdb5a7a3e5859128a1f4cb
Collins Oriakhi <collins.oriakhi@itara.co>
2022-03-29 00:36:24 +0100

    My Initial Commit for the Tic-Tac-Toe Game

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns:[GridItem]=[GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
+    @State private var move:[Move?]=Array(repeating: nil , count: 9)
+    @State private var isGamBoardDisabled=false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader{
+            geometry in  VStack {
+                Spacer()
+                LazyVGrid(columns: columns){
+                    ForEach(0..<9){ i in
+                        ZStack{
+                            Circle().frame(
+                                width: 80,
+                                height: 80
+                            ).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            
+                            Image(systemName: move[i]?.indicator ?? "")
+                                .resizable()
+                                .foregroundColor(.white)
+                                .frame(width:30, height: 30).foregroundColor(.white)
+                            
+                        }
+                        .onTapGesture{
+                            
+                            if isCellOcuppied(in: move, forIndex: i){
+                                return
+                                //                                Do mnot execute any code
+                            }
+                            move[i]=Move(player:  .human ,   cellIndex: i)
+                            isGamBoardDisabled = true
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                let computerPosition = determineComputerMovePosition(in: move)
+                                move[computerPosition] = Move(player: .computer, cellIndex: computerPosition)
+                                isGamBoardDisabled = false
+                                
+                            }
+                            
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .disabled(isGamBoardDisabled)
+        }
+    }
+    
+    func isCellOcuppied(in moves:[Move?], forIndex index:Int) ->  Bool {
+        return moves.contains(where: {$0?.cellIndex == index})
+    }
+    func determineComputerMovePosition(in moves:[Move?]) -> Int {
+        var newMovePositon = Int.random(in: 0..<9)
+        while isCellOcuppied(in: moves, forIndex: newMovePositon) {
+            newMovePositon = Int.random(in: 0..<9)
+            
+        }
+        return newMovePositon
+    }
+    
+    func checkWinCondition(in player:Player , in moves: [Move?]) -> Bool {
+        return false
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+//sparkles
+
+
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player:Player
+    let cellIndex:Int
+    
+    var indicator: String{
+        return player == .human ? "moon" : "sparkles"
     }
 }
